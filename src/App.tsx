99f5ec5297e48e75b0520397643abf8a75cd5b6d
Ley official <danyilfreelance@gmail.com>
2023-11-08 22:01:55 +0000

    initialisation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import './App.css'
 import {Route, Routes} from "react-router-dom";
 import Layout from "./Layout/Layout.tsx";
-import {Suspense} from "react";
 
 function App() {
   return (
     <>
         <Routes>
-            <Route path={'/'} element={<Suspense><Layout/></Suspense>}>
+            <Route path={'/'} element={<Layout/>}>
             </Route>
         </Routes>
     </>
