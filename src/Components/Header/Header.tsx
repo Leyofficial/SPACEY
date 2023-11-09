d8bd96d5d5accd45682f5d2d12f31ac0b14b60ae
Ley official <danyilfreelance@gmail.com>
2023-11-09 18:56:13 +0000

    added-bottom-header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,8 +1,24 @@
 import style from './Header.module.scss'
 import {CiFacebook, CiTwitter} from "react-icons/ci";
-import {AiOutlineInstagram, AiOutlineYoutube} from "react-icons/ai";
+import {AiOutlineInstagram, AiOutlineUser, AiOutlineYoutube} from "react-icons/ai";
+import logo from './../../assets/icons/logo.svg'
+import Input from "../../Utility/Input/Input.tsx";
+import {useState} from "react";
+import {Badge, IconButton} from "@mui/material";
+import {PiBasket} from "react-icons/pi";
+import {MdFavoriteBorder} from "react-icons/md";
 
 function Header() {
+    function notificationsLabel(count: number) {
+        if (count === 0) {
+            return 'no notifications';
+        }
+        if (count > 99) {
+            return 'more than 99 notifications';
+        }
+        return `${count} notifications`;
+    }
+    const [inputValue  , setValue ] = useState<string>('')
     return (
         <div className={style.container}>
             <div className={style.wrapper}>
@@ -22,8 +38,38 @@ function Header() {
                         </div>
                     </nav>
                 </div>
+                <div className={style.bottomBlock}>
+                    <nav className={style.nav}>
+                        <div className={style.titleBlock}>
+                            <img src={logo} alt="logo"/>
+                            <h1 className={style.title}>SPACEY</h1>
+                        </div>
+                        <div className={style.inputBlock}>
+                            <Input
+                                value={inputValue}
+                                callback={setValue}
+                                type={'text'}
+                                placeholder={'Search for anything...'}
+                                typeBtn={'search'}/>
+                        </div>
+                        <div className={style.actionBlock}>
+                            <IconButton onClick={() => console.log('b')} aria-label={notificationsLabel(100)}>
+                                <Badge badgeContent={1} color="info">
+                                    <PiBasket color={'white'}/>
+                                </Badge>
+                            </IconButton>
+                            <IconButton onClick={() => console.log('f')}>
+                                 <MdFavoriteBorder color={'white'}/>
+                            </IconButton>
+                            <IconButton onClick={() => console.log('p')}>
+                                <AiOutlineUser color={'white'}/>
+                            </IconButton>
+                        </div>
+                    </nav>
+                </div>
             </div>
         </div>
+
     )
 }
 
---
+++ src/Utility/CustomBtn/CustomBtn.tsx
@@ -0,0 +1,4 @@
+function CustomBtn() {
+
+}
+export default CustomBtn
