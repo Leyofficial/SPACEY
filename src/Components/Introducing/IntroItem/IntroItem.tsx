a83a3b629c79f9e9d8f4266f3824beb17da48b94
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-11-14 09:48:27 +0100

    added images and description about intro item

--- src/Components/Introducing/IntroItem/IntroItem.tsx
+++ src/Components/Introducing/IntroItem/IntroItem.tsx
@@ -1,9 +1,36 @@
 import style from './IntroItem.module.scss'
+import CustomBtn from "../../../Utility/CustomBtn/CustomBtn.tsx";
+import {ICategory} from "../../Header/Category/ProperWindow/PopperItem/PopperItem.tsx";
+import {useEffect, useState} from "react";
+import {getImageFromServer} from "../../../ApiRequests/uploads/getImage.ts";
+import {Skeleton} from "@mui/material";
 
-const IntroItem = () => {
+interface IIntroItemProps {
+    item: ICategory,
+    title: string
+}
+
+const IntroItem = ({item, title}: IIntroItemProps) => {
+    const [image, setImage] = useState<string | null>(null)
+const [loadImage,setLoadImage] = useState<boolean>(false)
+    useEffect(() => {
+        getImageFromServer(item?.product.images.mainImage, setImage,setLoadImage)
+
+    }, [item])
     return (
-        <div className={style.item}>
-
+        <div className={`${style.item} ${title === 'INTRODUCING' ? style.whiteBackground : ""}`}>
+            <div className={style.info}>
+                <p className={`${style.title} ${title === 'INTRODUCING' ? style.blue : ""}`}>{title}</p>
+                <h3 className={`${style.itemTitle} ${title === 'INTRODUCING' ? style.color : ""}`}>New {item?.product.title}</h3>
+                <p className={`${style.description} ${title === 'INTRODUCING' ? style.innerColor : ""}`}>{item?.product.description}</p>
+                <CustomBtn text={'SHOP NOW'}></CustomBtn>
+            </div>
+            <div className={style.coverImage}>
+                {loadImage ? <Skeleton  variant="text" width={240} height={240}></Skeleton> :  <img alt={'product'} src={image ? image : ""}/>}
+                {title !== 'INTRODUCING' ? <div className={style.price}>
+                    {item.product.price} $
+                </div> : null}
+            </div>
         </div>
     );
 };
--- src/Components/Introducing/Introducing.tsx
+++ src/Components/Introducing/Introducing.tsx
@@ -1,11 +1,14 @@
 import style from './Introducing.module.scss'
 import IntroItem from "./IntroItem/IntroItem.tsx";
+import {UseCustomQuery} from "../../ApiRequests/customQuery/customQuery.ts";
 
 const Introducing = () => {
+    const {data} = UseCustomQuery(`https://spacey-server.vercel.app/api/product?status=New`)
+
     return (
         <section className={style.container}>
-            <IntroItem></IntroItem>
-            <IntroItem></IntroItem>
+            <IntroItem title={"INTRODUCING"} item={data?.foundProduct[0]} ></IntroItem>
+            <IntroItem title={"INTRODUCING NEW"} item={data?.foundProduct[1]} ></IntroItem>
         </section>
     );
 };
