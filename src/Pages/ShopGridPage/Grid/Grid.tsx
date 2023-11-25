682244501a0bf483f49fa8a80ed1aac5e3362904
Ley official <danyilfreelance@gmail.com>
2023-11-25 15:44:07 +0000

    refactor v1

--- src/Pages/ShopGridPage/Grid/Grid.tsx
+++ src/Pages/ShopGridPage/Grid/Grid.tsx
@@ -1,112 +1,139 @@
-import style from './Grid.module.scss'
-import {ICategory} from "../../../types.ts";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import style from "./Grid.module.scss";
+import { ICategory } from "../../../types.ts";
 import SmallDealItem from "../../MainPage/Deals/SmallDeal/SmallDealItem.tsx";
-import {useEffect, useState} from "react";
-import {getAllItems} from "../../../ApiRequests/Items/Items.ts";
+import { getAllItems } from "../../../ApiRequests/Items/Items.ts";
 import SmallDealSkeleton from "../../MainPage/Deals/SmallDeal/SmallDealSkeleton.tsx";
-import {CustomSearch} from "../../../Utility/CustomSearch/CustomSearch.tsx";
-import {useLocation} from "react-router-dom";
-import {getSingleCategory} from "../../../ApiRequests/Items/getSingleCategory.ts";
-import {useGetParams} from "../../../hooks/params/getAllParams.ts";
+import { CustomSearch } from "../../../Utility/CustomSearch/CustomSearch.tsx";
+import { getSingleCategory } from "../../../ApiRequests/Items/getSingleCategory.ts";
+import { useGetParams } from "../../../hooks/params/getAllParams.ts";
+
+interface ISkeleton {
+    itemsOnScreen : number
+}
+
+function NotFound() {
+    return (
+        <div className={style.notFound}>
+            <p className={style.notFoundText}>
+                Product<span className={style.span}> not found</span> :(
+            </p>
+        </div>
+    );
+}
+
+function Skeleton({itemsOnScreen} : ISkeleton) {
+    return (
+        <div className={style.skeletonBlock}>
+            {Array(itemsOnScreen).fill(null).map(() => (
+                <SmallDealSkeleton />
+            ))}
+        </div>
+    );
+}
 
 function Grid() {
-    const location = useLocation()
-    const itemsOnScreen = 24
+    const location = useLocation();
+    const itemsOnScreen = 24;
     const [items, setItems] = useState<ICategory[]>([]);
-    const [valueInput, setValueInput] = useState<string>('')
+    const [valueInput, setValueInput] = useState<string>("");
     const [foundArrays, setFound] = useState<ICategory[]>([]);
-    const [isFound, setFoundItem] = useState(true)
+    const [isFound, setFoundItem] = useState(true);
+
     useEffect(() => {
-        const  {categoryParam , maxPriceParam , minPriceParam} = useGetParams();
-        if (!categoryParam) return
-        setFound([])
-        getSingleCategory(categoryParam).then(res => {
-            if (res.status === 'error') return;
+        const { categoryParam, maxPriceParam, minPriceParam } = useGetParams();
+        if (!categoryParam) return;
+
+        setFound([]);
+        getSingleCategory(categoryParam).then((res) => {
+            if (res.status === "error") return;
             if (minPriceParam && maxPriceParam) {
-                const priceFiltered = res.foundProduct.filter((item: any) => item.product.price <= maxPriceParam && item.product.price >= minPriceParam);
+                const priceFiltered = res.foundProduct.filter(
+                    (item: any) =>
+                        item.product.price <= maxPriceParam &&
+                        item.product.price >= minPriceParam
+                );
                 if (priceFiltered.length === 0) {
                     setFoundItem(false);
                 } else {
                     setFoundItem(true);
-                    setFound(priceFiltered.splice(0, itemsOnScreen));
+                    setFound(priceFiltered.slice(0, itemsOnScreen));
                 }
             } else {
                 setFound(res.foundProduct.slice(0, itemsOnScreen));
             }
-
-        })
-    }, [location.search]); // Подписываемся на изменение строки поиска
-
+        });
+    }, [location.search]);
 
     useEffect(() => {
-        getAllItems().then(res => setItems(res.data.categories))
+        getAllItems().then((res) => setItems(res.data.categories));
     }, []);
 
     useEffect(() => {
         if (items) {
-            setFound(items.slice(0, itemsOnScreen))
+            setFound(items.slice(0, itemsOnScreen));
         }
     }, [items]);
 
-
     useEffect(() => {
-        const  {categoryParam , maxPriceParam , minPriceParam} = useGetParams();
+        const { categoryParam, maxPriceParam, minPriceParam } = useGetParams();
 
-        if (!valueInput || !categoryParam) return
+        if (!valueInput || !categoryParam) return;
 
-        const foundItem: ICategory[] = items.filter((item: ICategory) => item.brand.toLowerCase().includes(valueInput.toLowerCase()));
-        const checkByCategory : ICategory[] = foundItem.filter((item: ICategory) => item.categoryOfProduct.includes(categoryParam));
-        const checkByPrice : ICategory[] = checkByCategory.filter((item : any) => {
+        const foundItem: ICategory[] = items.filter((item: ICategory) =>
+            item.brand.toLowerCase().includes(valueInput.toLowerCase())
+        );
+        const checkByCategory: ICategory[] = foundItem.filter((item: ICategory) =>
+            item.categoryOfProduct.includes(categoryParam)
+        );
+        const checkByPrice: ICategory[] = checkByCategory.filter((item: any) => {
             if (maxPriceParam && minPriceParam) {
-                return  item.product.price <= maxPriceParam && item.product.price >= minPriceParam
+                return (
+                    item.product.price <= maxPriceParam &&
+                    item.product.price >= minPriceParam
+                );
             }
-        })
+        });
 
         if (minPriceParam && maxPriceParam) {
             if (checkByPrice.length === 0) {
                 setFoundItem(false);
             } else {
                 setFoundItem(true);
-                setFound(checkByPrice.splice(0, itemsOnScreen));
+                setFound(checkByPrice.slice(0, itemsOnScreen));
             }
         } else {
-            if (checkByCategory && checkByCategory.length > 0) {
-                setFoundItem(true)
-            } else {
-                setFoundItem(false)
-            }
-            setFound(checkByCategory.slice(0, itemsOnScreen))
+            setFoundItem(checkByCategory && checkByCategory.length > 0);
+            setFound(checkByCategory.slice(0, itemsOnScreen));
         }
     }, [valueInput]);
 
-    function NotFound() {
-        return (
-            <div className={style.notFound}>
-                <p className={style.notFoundText}>
-                    Product<span className={style.span}> not found</span> :(
-                </p>
-            </div>
-        )
-    }
-    function Skeleton() {
-        return (
-            <div className={style.skeletonBlock}>
-                {Array(itemsOnScreen).fill(null).map(() => <SmallDealSkeleton/>)}
-            </div>
-        )
-    }
-
     return (
         <div className={style.block}>
-            <CustomSearch placeholder={'Search for brand...'} callback={setValueInput}/>
+            <CustomSearch
+                placeholder={"Search for brand..."}
+                callback={setValueInput}
+            />
 
-            {!isFound ? NotFound() : <div
-                className={style.items}> {foundArrays.length > 0 ? foundArrays?.map((item: ICategory, index: number) =>
-                <SmallDealItem key={index} item={item}></SmallDealItem>) : Skeleton()}</div>}
-            {/*{foundArrays.length > 0 ? foundArrays.map((item : ICategory , index : number) => <SmallDealItem key={index} item={item} /> ) : shuffledArray ?*/}
-            {/*    shuffledArray?.map((item: ICategory, index: number) => <SmallDealItem key={index} item={item}></SmallDealItem>) : Skeleton()}*/}
+            {!isFound ? (
+                <NotFound />
+            ) : (
+                <div className={style.items}>
+                    {foundArrays.length > 0 ? (
+                        foundArrays?.map((item: ICategory, index: number) => (
+                            <SmallDealItem
+                                key={index}
+                                item={item}
+                            ></SmallDealItem>
+                        ))
+                    ) : (
+                        <Skeleton itemsOnScreen={itemsOnScreen} />
+                    )}
+                </div>
+            )}
         </div>
-    )
+    );
 }
 
-export default Grid
+export default Grid;
