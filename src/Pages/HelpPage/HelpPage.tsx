288a638bafc4ccde715c195223e739022a05b5f3
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-11-27 21:06:12 +0100

    added assist items section

--- src/Pages/HelpPage/HelpPage.tsx
+++ src/Pages/HelpPage/HelpPage.tsx
@@ -1,6 +1,9 @@
 import style from './HelpPage.module.scss';
 import {IoSearch} from "react-icons/io5";
 import help from '../../assets/img/help.png'
+import {helpItems} from "./HelpItemsList.tsx";
+import AssistItem from "./AssistItem/AssistItem.tsx";
+
 const HelpPage = () => {
     return (
         <main className={style.container}>
@@ -10,7 +13,7 @@ const HelpPage = () => {
                         <p className={style.tag}>HELP CENTER</p>
                         <h1>How we can help you?</h1>
                         <div className={style.searchWrapper}>
-                            <IoSearch />
+                            <IoSearch/>
                             <input placeholder={'Enter your question or keyword'}/>
                             <button>SEARCH</button>
                         </div>
@@ -22,7 +25,11 @@ const HelpPage = () => {
                 </div>
             </header>
 
-            <section>
+            <section className={style.assistWrapper}>
+                <h1>What can we assist you with today?</h1>
+                <div className={style.assistItems}>
+                    {helpItems().map((item,index) => <AssistItem key={index} item={item}></AssistItem>)}
+                </div>
 
             </section>
         </main>
