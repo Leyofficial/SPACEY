12b74bb38c50ffda85c551a48773f1637a358ce6
Ley official <danyilfreelance@gmail.com>
2023-12-01 20:19:12 +0000

    added-relink-to-home

--- src/Pages/UserAccountPage/LoginPage/Login.tsx
+++ src/Pages/UserAccountPage/LoginPage/Login.tsx
@@ -5,7 +5,7 @@ import toast, {Toaster} from "react-hot-toast";
 import CustomTab from "../../../Utility/CustomTab/CustomTab.tsx";
 import {tabArray} from "../tabArray.ts";
 import {BsArrowRightShort} from "react-icons/bs";
-import {NavLink} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 import {useFormRegister} from "../../../hooks/auth/useFormRegister.ts";
 import axios from "axios";
 
@@ -16,18 +16,19 @@ interface MyForm {
 
 function Login() {
     const defaultValues  = ['email' , 'password'];
-   const {register , handleSubmit , errors , reset} = useFormRegister(defaultValues);
+    const navigate = useNavigate()
+   const {register , handleSubmit , errors } = useFormRegister(defaultValues);
     const submit: SubmitHandler<MyForm | any> = (dataFormInputs) => {
         axios
             .get(`https://spacey-server.vercel.app/auth?email=${dataFormInputs.email}&password=${dataFormInputs.password}`)
             .then((response) => {
                 localStorage.setItem('token' , response.data.token);
                 toast.success('Success');
+                navigate('/')
             })
             .catch((error) => {
                 const errorMessage = error.response ? error.response.data.message : 'Something went wrong ...';
                 toast.error(errorMessage);
-                reset()
             });
     };
 
