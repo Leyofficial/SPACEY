52250b2438d529633ff2fbc8d9de736293d84a1d
Ley official <danyilfreelance@gmail.com>
2023-12-05 19:23:03 +0000

    added-adapt-for-more-obj

--- src/Pages/Compare/Compare.tsx
+++ src/Pages/Compare/Compare.tsx
@@ -1,36 +1,64 @@
 import style from './Compare.module.scss'
 import Table from "../../Utility/Table/Table.tsx";
+
 function Compare() {
-    const columns = ['Customer feedback' , 'Price' ,'Sold by' , 'Brand' , 'Model' , 'Stock status' , 'Size' , 'Wight']
+    const columns = [{
+        text: 'Customer feedback',
+        accessor: 'rating'
+    },
+        {
+            text: 'Price',
+            accessor: 'price'
+        },
+        {
+            text: 'Sold by',
+            accessor: 'soldBy'
+        },
+        {
+            text: 'Brand',
+            accessor: 'brand'
+        },
+        {
+            text: 'Model',
+            accessor: 'model'
+        },
+        {
+            text: 'Stock status',
+            accessor: 'stockStatus',
+        },
+        {text: 'Size', accessor: 'size'},
+        {text: 'Weight', accessor: 'weight'},
+    ]
     const data = [{
         rating: '5star',
-        price : 100,
-        soldBy : 'Spacey',
-        brand : 'StarTech',
+        price: 100,
+        soldBy: 'Spacey',
+        brand: 'StarTech',
         model: 'iPhone 13',
-        stockStatus:  'In Stock',
+        stockStatus: 'In Stock',
         size: '6.1 inches',
-        weight : '194 grams',
+        weight: '194 grams',
     },
         {
             rating: '10star',
-            price : 100,
-            soldBy : 'Spacey',
-            brand : 'StarTech',
+            price: 100,
+            soldBy: 'Spacey',
+            brand: 'StarTech',
             model: 'iPhone 13',
-            stockStatus:  'In Stock',
+            stockStatus: 'In Stock',
             size: '6.1 inches',
-            weight : '194 grams',
+            weight: '194 grams',
         }
     ];
     return (
         <div className={style.block}>
             <div className={style.table}>
                 {columns.map((item) => {
-                   return <Table column={item} data={data} />
+                    return <Table column={item.text} accessor={item.accessor}  data={data}/>
                 })}
             </div>
         </div>
     )
 }
+
 export default Compare
--- src/Utility/Table/Table.tsx
+++ src/Utility/Table/Table.tsx
@@ -1,13 +1,11 @@
-const SimpleTable = ({column, data}) => (
-    <table style={{borderCollapse: 'collapse', width: '400px', marginBottom: '20px'}}>
+const SimpleTable = ({column, accessor ,data}) => (
+    <table style={{borderCollapse: 'collapse', width: '100%', marginBottom: '20px'}}>
         <tbody>
         <tr key={column}
-            style={{borderBottom: '1px solid #ddd', display: 'flex', justifyContent: 'space-between', padding: '1rem'}}>
-            <td>{column}</td>
+            style={{borderBottom: '1px solid #ddd', alignItems : 'center' , display: 'flex' , justifyContent: 'space-between', padding: '1rem'}}>
+            <td style={{width : '10rem'}}>{column}</td>
             {data.map(item =>
-                <>
-                    <td>{item.rating}</td>
-                </>
+                    <td>{item[accessor]}</td>
         )}
         </tr>
     </tbody>
