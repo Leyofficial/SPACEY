62486026ae322775cede971abec787d6802c9c06
Ley official <danyilfreelance@gmail.com>
2023-12-10 22:18:06 +0000

    fixed-billing-type

--- src/ApiRequests/Billing/Billing.ts
+++ src/ApiRequests/Billing/Billing.ts
@@ -2,30 +2,32 @@ import axios from "axios";
 import {IBillingFormValues, IProducts} from "../../Pages/PaymentPage/payment.types.ts";
 
 interface INewOrderData {
-    paymentType: string | null,
-    date: Date,
-    dataBilling:IBillingFormValues,
-    products: IProducts,
-    user: string,
-    orderId:string
+        paymentType: string | null,
+        date: Date,
+        dataBilling: IBillingFormValues,
+        products: IProducts[],
+        user: string,
+        orderId: string
 }
-export interface ICardDate{
 
-    number:string,
-    expiry:Date | string,
-    cvc:string | number,
-    name:string
+export interface ICardDate {
+
+    number: string,
+    expiry: Date | string,
+    cvc: string | number,
+    name: string
 
 
 }
 
 export const createNewOrder = async (data: INewOrderData) => {
-    return await axios.post(`https://spacey-server.vercel.app/processOrder`, {...data
+    return await axios.post(`https://spacey-server.vercel.app/processOrder`, {
+        ...data
     })
 }
-export const updatePaymentStatus = async (idOrderProcessing: string | undefined,cardDate : ICardDate) => {
-    return await axios.patch(`https://spacey-server.vercel.app/processOrder/${idOrderProcessing}`,{cardDate})
+export const updatePaymentStatus = async (idOrderProcessing: string | undefined, cardDate: ICardDate) => {
+    return await axios.patch(`https://spacey-server.vercel.app/processOrder/${idOrderProcessing}`, {cardDate})
 }
-export const deleteOrder = async (idOrder:string | undefined) => {
+export const deleteOrder = async (idOrder: string | undefined) => {
     return await axios.post(`https://spacey-server.vercel.app/orders/delete/${idOrder}`)
 }
