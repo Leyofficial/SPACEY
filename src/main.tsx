319a774d143a02ae9e3f29e5ecfe5c46790dad22
Ley official <danyilfreelance@gmail.com>
2023-12-11 18:28:31 +0000

    fixed-conflict

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,10 @@ import {QueryClient, QueryClientProvider} from "react-query";
 import {GoogleOAuthProvider} from "@react-oauth/google";
 import {Provider} from "react-redux";
 import {store} from "./redux/configureStore.ts";
-import 'tailwindcss/tailwind.css';
-import './index.css'
 const clientId = '982859489612-1o67pno0bhgh0dtvblloucbqbpjptlf5.apps.googleusercontent.com'
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-
         <BrowserRouter>
             <Provider store={store}>
             <QueryClientProvider client={queryClient}>
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import {defineConfig} from "vite";
-import tailwindcss from "tailwindcss";
 import react from "@vitejs/plugin-react";
 import {watch} from "fs";
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  },
   plugins: [react(),
     {name:'watch-external',
       configureServer(server){
