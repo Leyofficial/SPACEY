489da206408812731611e6e097285c0a51dab29c
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-12-11 19:09:06 +0100

    config vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { watch } from 'fs'
-// https://vitejs.dev/config/
+import {defineConfig} from "vite";
+import tailwindcss from "tailwindcss";
+import react from "@vitejs/plugin-react";
+import {watch} from "fs";
+
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
   plugins: [react(),
     {name:'watch-external',
-    configureServer(server){
-      const filesToWatch = []
+      configureServer(server){
+        const filesToWatch = []
 
-      const handleChange = () => server.ws.send({type:'full-reload'})
+        const handleChange = () => server.ws.send({type:'full-reload'})
 
-      for(const file of filesToWatch){
-        watch(file,handleChange)
-      }
-    }}
+        for(const file of filesToWatch){
+          watch(file,handleChange)
+        }
+      }}
   ],
-})
+});
