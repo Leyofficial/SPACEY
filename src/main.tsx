49eeef60eb4e2cd4b664037b10577dcbffb436c1
Ley official <danyilfreelance@gmail.com>
2023-12-11 17:40:39 +0000

    added-tailwind-structure

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import {QueryClient, QueryClientProvider} from "react-query";
 import {GoogleOAuthProvider} from "@react-oauth/google";
 import {Provider} from "react-redux";
 import {store} from "./redux/configureStore.ts";
+import 'tailwindcss/tailwind.css';
+import './index.css'
 const clientId = '982859489612-1o67pno0bhgh0dtvblloucbqbpjptlf5.apps.googleusercontent.com'
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
