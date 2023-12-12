94bfccb85e5962d4c0326e01cc7564a82558e29c
temcenkovolo <temcenkovova8@gmail.com>
2023-12-12 19:51:46 +0100

    added blur on popper category

--- src/Components/Header/Category/Category.tsx
+++ src/Components/Header/Category/Category.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import style from './Category.module.scss'
 import {AiOutlineArrowDown, AiOutlineArrowUp} from "react-icons/ai";
 import {NavLink} from "react-router-dom";
@@ -6,17 +6,31 @@ import {CategoryData} from "./CategoryData.tsx";
 import PopperWindow from "./ProperWindow/Left/PopperWindow.tsx";
 
 function Category() {
-    const [open , setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
+    const blurPopperWindow = useRef<HTMLDivElement>(null)
 
     function handleClick() {
         setOpen((prev) => !prev)
     }
 
+    useEffect(() => {
+        const handeClickOutside = (e: MouseEvent) => {
+            if (blurPopperWindow.current?.contains(e.target as Node)) {
+                return
+            }
+            setOpen(false)
+        }
+        document.addEventListener("mousedown", handeClickOutside)
+        return () => {
+            document.removeEventListener("mousedown", handeClickOutside)
+        }
+    }, [])
+
     return (
         <div className={style.container}>
             <div className={style.block}>
                 <div className={style.popper}>
-                    <button className={`${style.btn} ${open ? style.active : null}`}  type="button"
+                    <button className={`${style.btn} ${open ? style.active : null}`} type="button"
                             onClick={handleClick}>
                         All Category
                         {open ? <AiOutlineArrowUp size={15} color={'white'}/> :
@@ -24,7 +38,7 @@ function Category() {
                     </button>
                 </div>
                 <div className={style.navItems}>
-                    {CategoryData.map((item  , index : number) => {
+                    {CategoryData.map((item, index: number) => {
                         return <NavLink key={index} className={`${style.btn}`} to={item.path}>
                             <div className={style.icon}>
                                 {item.icon}
@@ -38,7 +52,7 @@ function Category() {
             </div>
             <div className={style.popperBlock}>
                 <div className={style.boxBlock}>
-                    {open ? <PopperWindow/> : null}
+                    {open ? <PopperWindow refHandler={blurPopperWindow}/> : null}
                 </div>
             </div>
         </div>
