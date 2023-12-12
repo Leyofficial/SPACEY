e22d6dca1f8a5670edbbe8c77c494c46ec9ef1ad
Ley official <danyilfreelance@gmail.com>
2023-12-12 22:56:15 +0000

    added-different-style

--- src/Pages/UserAccountPage/DashBoardPage/DashBoardPage.tsx
+++ src/Pages/UserAccountPage/DashBoardPage/DashBoardPage.tsx
@@ -10,33 +10,33 @@ import CustomizedTables, {ICustomTable} from "./CustomTable/CustomTable.tsx";
 
 function DashBoardPage() {
     const {user} = useAppSelector((state) => state.user);
-    const orderInfo : IOrderInfo[] = [
+    const orderInfo: IOrderInfo[] = [
         {
-            text : 'Total Orders',
-            background : '#EAF6FE',
-            icon : <MdOutlineRocket  size={'2rem'} color={'#2DA5F3'}/>,
-            numberOfOrders : 154
-        } ,
-        {
-            text : 'Pending Orders',
-            background : '#FFF3EB',
-            icon : <PiReceiptDuotone  size={'2rem'} color={'#FA8232'}/>,
-            numberOfOrders : 5
+            text: 'Total Orders',
+            background: '#EAF6FE',
+            icon: <MdOutlineRocket size={'2rem'} color={'#2DA5F3'}/>,
+            numberOfOrders: 154
         },
         {
-            text : 'Completed Orders',
-            background : '#EAF7E9',
-            icon : <BiPackage   size={'2rem'} color={'#2DB324'}/>,
-            numberOfOrders : 51
+            text: 'Pending Orders',
+            background: '#FFF3EB',
+            icon: <PiReceiptDuotone size={'2rem'} color={'#FA8232'}/>,
+            numberOfOrders: 5
+        },
+        {
+            text: 'Completed Orders',
+            background: '#EAF7E9',
+            icon: <BiPackage size={'2rem'} color={'#2DB324'}/>,
+            numberOfOrders: 51
         }
     ]
-    const tableInfo : ICustomTable[]  = [
+    const tableInfo: ICustomTable[] = [
         {
-            orderId : '#96459765',
-            status : 'In progress',
-            date : 'Dec 30, 2019 05:18',
-            total : '$1,500 (10 Products)',
-            pathForLink : '/'
+            orderId: '#96459765',
+            status: 'In progress',
+            date: 'Dec 30, 2019 05:18',
+            total: '$1,500 (10 Products)',
+            pathForLink: '/'
         }
     ]
 
@@ -57,31 +57,42 @@ function DashBoardPage() {
                                 {user.picture ? <Avatar alt="Remy Sharp" src={user?.picture}/> : <Avatar/>}
                             </div>
                             <div className={style.avatarText}>
-                                <h2>{user.givenName ?  user.givenName + ' ' + user.familyName : <Skeleton variant={'rounded'} width={'7rem'} height={15}/>}</h2>
+                                <h2>{user.givenName ? user.givenName + ' ' + user.familyName :
+                                    <Skeleton variant={'rounded'} width={'7rem'} height={15}/>}</h2>
                                 <p>{user.locale || 'unknown'}</p>
                             </div>
                         </div>
                         <div className={style.otherContacts}>
                             <div className={style.item}>
-                                <p>Email</p> : {user.email ? <span>{user.email}</span> : <Skeleton variant={'rounded'} width={'10rem'}/>}
+                                <p>Email</p> : {user.email ? <span>{user.email}</span> :
+                                <Skeleton variant={'rounded'} width={'10rem'}/>}
+                            </div>
+                            <div className={style.item}>
+                                <p>Phone</p> : {!user ? <span>{user}</span> : <span>unknown</span>}
                             </div>
                         </div>
                     </FormInfo>
                     <FormInfo text={'Billing address'}>
                         <div className={style.billingAddress}>
                             <p className={style.name}>{user.givenName} {user.familyName}</p>
-                            <p className={style.billingText}>East Tejturi Bazar, Word No. 04, Road No. 13/x, House no. 1320/C, Flat No. 5D, Dhaka - 1200, Bangladesh</p>
+                            <p className={style.billingText}>East Tejturi Bazar, Word No. 04, Road No. 13/x, House no.
+                                1320/C, Flat No. 5D, Dhaka - 1200, Bangladesh</p>
                         </div>
                     </FormInfo>
                     <div className={style.statsOrders}>
-                        {orderInfo.map((item : IOrderInfo) =>
-                        <OrderInfo background={item.background} numberOfOrders={item.numberOfOrders} icon={item.icon} text={item.text}/>
+                        {orderInfo.map((item: IOrderInfo) =>
+                            <OrderInfo background={item.background} numberOfOrders={item.numberOfOrders}
+                                       icon={item.icon} text={item.text}/>
                         )}
                     </div>
                 </div>
-                <div className={style.table}>
-                    <CustomizedTables array={tableInfo}/>
+                <div className={style.tableBlock}>
+                    <h2 className={style.tableTitle}>Recent Order</h2>
+                    <div className={style.table}>
+                        <CustomizedTables array={tableInfo}/>
+                    </div>
                 </div>
+
             </div> : null
     )
 }
