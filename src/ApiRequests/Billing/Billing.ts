7831619fcd1304e1346b01c1f4171edf4b636cf9
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-12-16 18:14:00 +0100

    ad

--- src/ApiRequests/Billing/Billing.ts
+++ src/ApiRequests/Billing/Billing.ts
@@ -5,32 +5,40 @@ import {IBillingFormValues, IProducts} from "../../Pages/PaymentPage/payment.typ
 interface INewOrderData {
     paymentType: string | null,
     date: Date,
-    dataBilling:IBillingFormValues,
+    dataBilling: IBillingFormValues,
     products: IProducts[],
     user: string,
-    orderId:string
+    orderId: string
 }
-export interface ICardDate{
 
-    number:string,
-    expiry:Date | string,
-    cvc:string | number,
-    name:string
+export interface ICardDate {
+
+    number: string,
+    expiry: Date | string,
+    cvc: string | number,
+    name: string
 
 
 }
 
 export const createNewOrder = async (data: INewOrderData) => {
-    return await axios.post(`https://spacey-server.vercel.app/processOrder`, {...data
+    return await axios.post(`https://spacey-server.vercel.app/processOrder`, {
+        ...data
     })
 }
-export const updatePaymentStatus = async (idOrderProcessing: string | undefined,cardDate : ICardDate) => {
-    return await axios.patch(`https://spacey-server.vercel.app/processOrder/${idOrderProcessing}`,{cardDate})
+export const updatePaymentStatus = async (idOrderProcessing: string | undefined, cardDate: ICardDate) => {
+    return await axios.patch(`https://spacey-server.vercel.app/processOrder/${idOrderProcessing}`, {cardDate})
 }
-export const deleteOrder = async (idOrder:string | undefined) => {
+export const deleteOrder = async (idOrder: string | undefined) => {
     return await axios.post(`https://spacey-server.vercel.app/orders/delete/${idOrder}`)
 }
 
-export const saveCard = async (idUser:string,paymentCard:ICardDate) => {
-    return await axios.patch(`https://spacey-server.vercel.app/auth/card/${idUser}`,paymentCard)
+export const saveCard = async (idUser: string, paymentCard: ICardDate) => {
+    const data = {
+        number: paymentCard.number,
+        name: paymentCard.name,
+        cvc: paymentCard.cvc,
+        expiry: paymentCard.expiry
+    }
+    return await axios.patch(`https://spacey-server.vercel.app/auth/card/${idUser}`, data)
 }
