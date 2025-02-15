3dd7c7c0af5503fc711d5e92084117fc0a4500df
Ley official <danyilfreelance@gmail.com>
2025-02-15 11:24:57 +0000

    fixed Card totals calculations

--- src/Pages/PaymentPage/ShoppingCart/CardTotals/CardTotals.tsx
+++ src/Pages/PaymentPage/ShoppingCart/CardTotals/CardTotals.tsx
@@ -1,47 +1,60 @@
-import style from './CardTotals.module.scss';
+import style from "./CardTotals.module.scss";
 import CustomBtn from "../../../../Utility/CustomBtn/CustomBtn.tsx";
-import {useEffect, useState} from "react";
-import {ICardTotalItems} from "../shoppingCartTypes.ts";
-import {useAppSelector} from "../../../../redux/hooks/hooks.ts";
+import { useEffect, useState } from "react";
+import { ICardTotalItems } from "../shoppingCartTypes.ts";
+import { useAppSelector } from "../../../../redux/hooks/hooks.ts";
 
+const CardTotals = ({ totalData }: ICardTotalItems) => {
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+  const { user } = useAppSelector((state) => state.user);
 
-const CardTotals = ({totalData}:ICardTotalItems ) => {
-    const [totalPrice, setTotalPrice] = useState<number>(0)
-    const {user} = useAppSelector(state => state.user)
-    useEffect(() => {
+  useEffect(() => {
+    if (totalData) {
+      const total = totalData.reduce((sum, item) => {
+        return sum + Number(item.count) * Number(item.price);
+      }, 0);
+      setTotalPrice(Number(total));
+    }
+  }, [totalData]);
 
-        if (totalData) {
-            const total = totalData.reduce((sum, item) => {
-                return sum + Number(item.count) * Number(item.price)
-            }, 0)
-            setTotalPrice(Number(total))
-        }
+  const discount = totalPrice / 10;
+  const tax = totalPrice / 50;
+  const finalTotal = totalPrice - discount + tax;
 
-    }, [totalData])
-
-
-    return (
-        <section className={style.cardTotal}>
-            <div className={style.wrapperInfo}>
-                <h1>Card Totals</h1>
-                <div className={style.info}>
-                    <p><span>Sub total</span>${totalPrice}.00</p>
-                    <p><span>Shipping</span>Free</p>
-                    <p><span>Discount</span>${totalPrice / 10}.00</p>
-                    <p><span>Tax</span>${totalPrice / 50}.00</p>
-                </div>
-                <div className={style.total}>
-                    <p><span>Total</span>${(totalPrice + (totalPrice / 10) + (totalPrice / 50))}</p>
-                </div>
-
-                <div className={style.btn}>
-                    <CustomBtn blockWidth={'18rem'} disable={!totalData} path={totalData ? `/payment-grid/${user?._id}` : '#'} text={'proceed to checkout'}></CustomBtn>
-                </div>
-            </div>
-
-
-        </section>
-    );
+  return (
+    <section className={style.cardTotal}>
+      <div className={style.wrapperInfo}>
+        <h1>Card Totals</h1>
+        <div className={style.info}>
+          <p>
+            <span>Sub total</span>${totalPrice.toFixed(2)}
+          </p>
+          <p>
+            <span>Shipping</span>Free
+          </p>
+          <p>
+            <span>Discount</span>${discount.toFixed(2)}
+          </p>
+          <p>
+            <span>Tax</span>${tax.toFixed(2)}
+          </p>
+        </div>
+        <div className={style.total}>
+          <p>
+            <span>Total</span>${finalTotal.toFixed(2)}
+          </p>
+        </div>
+        <div className={style.btn}>
+          <CustomBtn
+            blockWidth={"18rem"}
+            disable={!totalData}
+            path={totalData ? `/payment-grid/${user?._id}` : "#"}
+            text={"proceed to checkout"}
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default CardTotals;
